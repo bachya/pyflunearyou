bbe35f5285235e22ba8b3f5af8186740715256d4
Aaron Bach <bachya1208@gmail.com>
2022-05-09 22:23:49 -0600

    Move coverage configuration to pyproject.toml (#77)
    * Move coverage configuration to pyproject.toml
    
    * Fix

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ def coverage(session: nox.sessions.Session) -> None:
     """Report test coverage."""
     args = session.posargs or [
         "-s",
-        "--cov=aionotion",
+        "--cov=pyflunearyou",
         "--cov-report=term-missing",
         "--cov-report=xml",
         "tests/",
